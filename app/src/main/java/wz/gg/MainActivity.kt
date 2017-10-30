64aef990f269efbd2bc4208e68b34c3c85f221e4
1031066280@qq.com <1031066280@qq.com>
2017-10-30 15:46:43 +0800

    123

--- app/src/main/java/wz/gg/MainActivity.kt
+++ app/src/main/java/wz/gg/MainActivity.kt
@@ -4,25 +4,40 @@ import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
+import android.webkit.WebView
+
+
 
 class MainActivity : AppCompatActivity() {
-    var model:KeyModel?=null
+    var model: KeyModel? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        model=intent.getSerializableExtra("model") as KeyModel
+        model = intent.getSerializableExtra("model") as KeyModel
         main_web.loadUrl(model!!.appForwardurl); // 你要回到的那个首页的URL
-        tag1_tv.text=model!!.navigationTitle1
-        tag2_tv.text=model!!.navigationTitle2
+        tag1_tv.text = model!!.navigationTitle1
+        tag2_tv.text = model!!.navigationTitle2
         tag1_tv.setOnClickListener {
-            val viewIntent = Intent("android.intent.action.VIEW", Uri.parse(model!!.navigationUrl1))
-            startActivity(viewIntent)
+            main_web.loadUrl(model!!.navigationUrl1); // 你要回到的那个首页的URL
+
+//            val viewIntent = Intent("android.intent.action.VIEW", Uri.parse(model!!.navigationUrl1))
+//            startActivity(viewIntent)
         }
         tag2_tv.setOnClickListener {
-            val viewIntent = Intent("android.intent.action.VIEW", Uri.parse(model!!.navigationUrl2))
-            startActivity(viewIntent)
+            main_web.loadUrl(model!!.navigationUrl2); // 你要回到的那个首页的URL
+
+//            val viewIntent = Intent("android.intent.action.VIEW", Uri.parse(model!!.navigationUrl2))
+//            startActivity(viewIntent)
         }
+        main_web.setWebViewClient(WebViewClient());
+        main_web.setWebViewClient(object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean { //  重写此方法表明点击网页里面的链接还是在当前的webview里跳转，不跳到浏览器那边
+                view.loadUrl(url)
+                return true
+            }
+        })
         home_iv.setOnClickListener {
             main_web.clearHistory(); // 清除
             main_web.loadUrl(model!!.appForwardurl); // 你要回到的那个首页的URL
